4e0295939117ab9be5288c97a8ae82d830ced822
george-silviu <costache.georgesilviu@yahoo.ro>
2024-02-14 10:47:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryItem from "./components/category-item/category-item.component";
+
+import "./categories.styles.scss";
 
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Aventura",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "Clasici",
+    },
+    {
+      id: 3,
+      title: "Educatie",
+    },
+    {
+      id: 4,
+      title: "Poezie",
+    },
+    {
+      id: 5,
+      title: "Nuvele",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
+      })}
     </div>
   );
 }
