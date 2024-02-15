fe50ccc483dfada6839080644aecd3a270a6c88c
george-silviu <costache.georgesilviu@yahoo.ro>
2024-02-15 10:54:06 +0200

    setup react router navbar and pages display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import CategoryList from "./components/category-list/category-list.component";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop page</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <CategoryList />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
