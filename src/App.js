be01979ab1bbcf9b3d746d2eaeefa8bba89bc5bf
george-silviu <costache.georgesilviu@yahoo.ro>
2024-02-16 11:08:29 +0200

    added navigation bar and links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return (
