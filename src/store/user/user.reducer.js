024fecc98e7c6be0846757d903fdbec6b25dadc5
george-silviu <costache.georgesilviu@yahoo.ro>
2024-03-19 16:41:05 +0200

    migrated user reducer to createSlice

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,19 +1,20 @@
-import { USER_ACTION_TYPES } from "./user.types";
+import { createSlice } from "@reduxjs/toolkit";
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
+//this replaces user.action.js, user.types.js, and user.reducer.js
+export const userSlice = createSlice({
+  name: "user",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload; //this code looks like a mutation, but it's actually using Immer behind the scenes
+    },
+  },
+});
 
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      };
-    default:
-      return state; //if the action type is not handled, return the current state
-  }
-};
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
