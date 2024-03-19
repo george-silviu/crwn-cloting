e1e8b068a970003919f05a1838ced65417874cde
george-silviu <costache.georgesilviu@yahoo.ro>
2024-03-19 16:20:37 +0200

    migrating from redux to @reduxjs/toolkit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { store, persistor } from "./store/store";
+import { store } from "./store/store";
 
 import "./index.scss";
 
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,31 +1,39 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import localStorage from "redux-persist/lib/storage"; // defaults to localStorage for web
+/**
+ * Redux toolkit includes redux-thunk by default, so you don't need to install it separately.
+ */
+
+//import { compose, createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
+// import localStorage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-//redux-persist config
-const persistConfig = {
-  key: "root",
-  storage: localStorage,
-  blacklist: ["user"],
-};
+// //redux-persist config
+// const persistConfig = {
+//   key: "root",
+//   storage: localStorage,
+//   blacklist: ["user"],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
-); //run only when not in production
+);
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(persistedReducer, undefined, composeEnhancers);
+export const store = configureStore({
+  reducer: rootReducer,
+  //middleware: middleWares,
+});
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
